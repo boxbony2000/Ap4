b0ba4f086a97e45c2fa9794a151ad487c1dfc167
luisangel2000 <luisangelsqgredoramirez342@gmail.com>
2022-07-05 17:54:43 -0500

    Ap4 empezando

---
+++ pantallas/SRadd.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {useState} from 'react'
+import { ScrollView, Button, View, StyleSheet, TextInput } from 'react-native';
+
+const SRadd = () => {
+
+    const styles = StyleSheet.create({
+    });  
+
+    return (
+        <ScrollView>
+
+        </ScrollView>
+    )
+}
+
+export default SRadd
