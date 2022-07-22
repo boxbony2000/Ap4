e280111f735f2505e712d73301b1adf90e151043
luisangel2000 <luisangelsqgredoramirez342@gmail.com>
2022-07-22 18:36:37 -0500

    inicio de sesion con exito

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import * as React from 'react';
-import { Button, View, StyleSheet } from 'react-native';
+import React from 'react';
+import {useState} from 'react'
+import { Button, View, StyleSheet, Text, TextInput,} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 import SRadd from './pantallas/SRadd';
 import SRread from './pantallas/SRread';
@@ -20,7 +22,80 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  Boton: {
+    margin: 10,
+    minWidth: "40%",
+    textAlign: "center",
+  },
+  Titulo: {
+    fontWeight: 'bold',
+    margin: 50,
+    fontSize: 30,
+    color: '#f51818'
+  },
 });
+
+function LoginScreen({ navigation }) {
+
+  const [Usuario, setusuario]=useState({
+    usuario:'',
+    pass:'',
+  })
+
+  const capturar =(atrib,valor) =>{
+    setusuario({...Usuario,[atrib]:valor})
+    }
+
+  function verificar(){
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, Usuario.usuario, Usuario.pass)
+    .then((userCredential) => {
+      navigation.navigate('Home')
+    })
+    .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(errorMessage)
+    });
+  }
+
+
+  return (
+    
+    <View style={styles.container}>
+    <Text  style={styles.Titulo} >Boutique Online</Text>
+    <TextInput
+        style={styles.input}
+        placeholder="Correo"
+        onChangeText={(value)=>capturar('usuario',value)}
+      />
+    <TextInput
+        style={styles.input}
+        placeholder="Contraseña"
+        secureTextEntry={true}
+        onChangeText={(value)=>capturar('pass',value)}
+      />
+
+   
+    
+    <View  style={styles.Boton}>
+      <Button
+        title="ingresar"
+        color="#000000"
+        onPress={() =>verificar() }
+      />
+    </View>
+
+    
+    </View>
+  );
+}
 
 function HomeScreen({ navigation }) {
   return (
@@ -93,14 +168,13 @@ const Stack = createNativeStackNavigator();
 function SRMain() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Entrada">
+        <Stack.Screen name="Inicio de Sesión" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
-
         <Stack.Screen name="Agregar" component={AddScreen} />
         <Stack.Screen name="Ver" component={ReadScreen} />
         <Stack.Screen name="Actualizar" component={UpdateScreen} />
         <Stack.Screen name="Eliminar" component={DeleteScreen} />
-
       </Stack.Navigator>
     </NavigationContainer>
   );
